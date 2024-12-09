507686cf44c1f2301af05abdcc9fe520821df6f9
JonathanBetPer <jonathanbetper@gmail.com>
2024-12-09 16:16:47 +0000

    Planteamiento JS

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const usuarios = [];
+
+document.getElementById("add-button").addEventListener("click", addUsuario);
+document.getElementById("search-button").addEventListener("click", searchUsuario);
+
+function addUsuario() {
+
+}
+
+function validateName(name) {
+
+}
+
+function validateEmail(email) {
+
+}
+
+function validateAge(age) {
+
+}
+
+function mostrarUsuario(usuario) {
+
+}
+
+function deleteUsuario(email) {
+
+}
+
+function mostrarUsuarioList() {
+
+}
+
+function limpiarForm() {
+
+}
+
+function searchUsuario() {
+
+}
